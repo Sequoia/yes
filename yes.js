a0494fa55f3472ac16663e537ca6b806a9975574
contra <contra@users.noreply.github.com>
2017-06-14 12:12:57 -0400

    fix posix compat

--- yes.js
+++ yes.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
-const buf = 'y\n'.repeat(4096)
+var msg = process.argv.length > 2 ? process.argv[2] : 'y';
+var buf = msg + '\n'.repeat(4096);
+
 while (1){
   if (!process.stdout.write(buf)) process.exit(0);
 }
