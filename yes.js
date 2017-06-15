a3f981177c79a52976071f3260923b729cf249dd
Renée Kooi <renee@kooi.me>
2017-06-15 16:28:29 +0200

    Use `fs` to write to stdout
    `process.stdout.write` writes to a Node.js stream, so it does a bunch of
    work before anything is actually written. This patch uses `fs.writeSync`
    to write to the stdout fd.
    
    If you write a lot of stuff very fast `writeSync` will start throwing
    EAGAIN errors. There might be a better way to wait for the file to be
    writable again, but this patch just catches those errors and ignores
    them.
    
    This speeds it up by about 7x for me, from just below 500KiB/s to
    3.5~4MiB/s.

--- yes.js
+++ yes.js
@@ -1,9 +1,21 @@
 #!/usr/bin/env node
 var msg = process.argv.length > 2 ? process.argv[2] : 'y';
+var fs = require('fs')
+var stdout = process.stdout.fd
 
-while(1){
-  if(!process.stdout.write(msg + '\r\n')){
-    process.exit(0);
+var buf = Buffer.from(msg + '\n')
+var len = buf.length
+var remaining = len
+
+while (true) {
+  try {
+    var written = fs.writeSync(stdout, buf, null, remaining)
+    remaining -= written
+  } catch (err) {
+    // catch EAGAIN
+    if (err.code !== 'EAGAIN')
+      process.exit(1)
   }
-}
 
+  if (remaining === 0) remaining = len
+}
