cdf1e123e37d645bb8691fbab82ab483ac6a975f
Renée Kooi <renee@kooi.me>
2017-06-15 16:40:03 +0200

    Buffer writes

--- yes.js
+++ yes.js
@@ -3,7 +3,9 @@ var msg = process.argv.length > 2 ? process.argv[2] : 'y';
 var fs = require('fs')
 var stdout = process.stdout.fd
 
-var buf = Buffer.from(msg + '\n')
+var BUFSIZ = 8192
+var repeats = Math.floor(BUFSIZ / (msg.length + 1))
+var buf = Buffer.from((msg + '\n').repeat(repeats))
 var len = buf.length
 var remaining = len
 
